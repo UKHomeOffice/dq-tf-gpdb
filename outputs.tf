c26ceda1cf2e1bd2be5ad0d429f67884dae71ab6
david-wirelab <info@wirelab.co.uk>
2017-12-06 10:30:10 +0000

    fix fmt

--- outputs.tf
+++ outputs.tf
@@ -10,30 +10,30 @@ output "segment_sg_id" {
   value = "${aws_security_group.segment_sg.id}"
 }
 
-output  "gpdb_master1_ip" {
+output "gpdb_master1_ip" {
   value = "${var.gpdb_master1_ip}"
 }
 
-output  "gpdb_master2_ip" {
+output "gpdb_master2_ip" {
   value = "${var.gpdb_master2_ip}"
 }
 
-output  "gpdb_segment1_ip" {
+output "gpdb_segment1_ip" {
   value = "${var.gpdb_segment1_ip}"
 }
 
-output  "gpdb_segment2_ip" {
+output "gpdb_segment2_ip" {
   value = "${var.gpdb_segment2_ip}"
 }
 
-output  "gpdb_segment3_ip" {
+output "gpdb_segment3_ip" {
   value = "${var.gpdb_segment3_ip}"
 }
 
-output  "gpdb_segment4_ip" {
+output "gpdb_segment4_ip" {
   value = "${var.gpdb_segment4_ip}"
 }
 
-output  "gpdb_segment5_ip" {
+output "gpdb_segment5_ip" {
   value = "${var.gpdb_segment5_ip}"
 }
