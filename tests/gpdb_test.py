0a209da7ddd18cfcbee8b26ca498a4fd24960a93
nick-otter <nicholas.a.otter@gmail.com>
2018-01-08 16:45:36 +0000

    added naming suffix to main, deleted unused vars

--- tests/gpdb_test.py
+++ tests/gpdb_test.py
@@ -38,7 +38,7 @@ class TestE2E(unittest.TestCase):
                 gpdb_segment4_ip              = "10.1.2.24"
                 gpdb_segment5_ip              = "10.1.2.25"
                 az                            = "eu-west-2a"
-                name_prefix                   = "dq-"
+                naming_suffix                = "apps-preprod-dq"
             }
         """
         self.result = Runner(self.snippet).result
@@ -49,14 +49,14 @@ class TestE2E(unittest.TestCase):
     def test_az_dq_database(self):
         self.assertEqual(self.result['gpdb']["aws_subnet.dq_database"]["availability_zone"], "eu-west-2a")
 
-    def test_name_prefix_dq_database(self):
-        self.assertEqual(self.result['gpdb']["aws_subnet.dq_database"]["tags.Name"], "dq-apps-gpdb-subnet")
+    def test_name_suffix_dq_database(self):
+        self.assertEqual(self.result['gpdb']["aws_subnet.dq_database"]["tags.Name"], "subnet-gpdb-apps-preprod-dq")
 
-    def test_name_prefix_master_sg(self):
-        self.assertEqual(self.result['gpdb']["aws_security_group.master_sg"]["tags.Name"], "dq-apps-gpdb-master-sg")
+    def test_name_suffix_master_sg(self):
+        self.assertEqual(self.result['gpdb']["aws_security_group.master_sg"]["tags.Name"], "sg-master-gpdb-apps-preprod-dq")
 
     def test_name_prefix_segment_sg(self):
-        self.assertEqual(self.result['gpdb']["aws_security_group.segment_sg"]["tags.Name"], "dq-apps-gpdb-segment-sg")
+        self.assertEqual(self.result['gpdb']["aws_security_group.segment_sg"]["tags.Name"], "sg-segment-gpdb-apps-preprod-dq")
 
 if __name__ == '__main__':
     unittest.main()
