6246ffa9239116848fed3003328891d5a6538617
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 13:39:18 +0000

    use real greenplum

--- tests/gpdb_test.py
+++ tests/gpdb_test.py
@@ -46,17 +46,57 @@ class TestE2E(unittest.TestCase):
     def test_root_destroy(self):
         self.assertEqual(self.result["destroy"], False)
 
-    def test_az_dq_database(self):
-        self.assertEqual(self.result['gpdb']["aws_subnet.dq_database"]["availability_zone"], "eu-west-2a")
+    def test_az_dq_subnet_0(self):
+        self.assertEqual(self.result['gpdb']["aws_subnet.subnets.0"]["availability_zone"], "eu-west-2a")
 
-    def test_name_suffix_dq_database(self):
-        self.assertEqual(self.result['gpdb']["aws_subnet.dq_database"]["tags.Name"], "subnet-gpdb-apps-preprod-dq")
+    def test_az_dq_subnet_1(self):
+        self.assertEqual(self.result['gpdb']["aws_subnet.subnets.1"]["availability_zone"], "eu-west-2a")
+
+    def test_az_dq_subnet_2(self):
+        self.assertEqual(self.result['gpdb']["aws_subnet.subnets.2"]["availability_zone"], "eu-west-2a")
+
+    def test_az_dq_subnet_3(self):
+        self.assertEqual(self.result['gpdb']["aws_subnet.subnets.3"]["availability_zone"], "eu-west-2a")
+
+    # def test_master_1(self): @TODO: fix
+    # self.assertTrue(Runner.finder(self.result['gpdb']["aws_network_interface.master_1_0"], 'private_ips', "10.1.25.101"))
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_1_0"]["private_ips.#"], "10.1.25.101")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_1_1"]["private_ips.#"], "10.1.26.101")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_1_2"]["private_ips.#"], "10.1.27.105")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_1_3"]["private_ips.#"], "10.1.28.229")
+    #
+    # def test_master_2(self):
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_2_0"]["private_ips.#"], "10.1.25.4")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_2_1"]["private_ips.#"], "10.1.26.234")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_2_2"]["private_ips.#"], "10.1.27.183")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.master_2_3"]["private_ips.#"], "10.1.28.86")
+    #
+    # def test_segment_1(self):
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_1_0"]["private_ips.#"], "10.1.25.224")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_1_1"]["private_ips.#"], "10.1.26.66")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_1_2"]["private_ips.#"], "10.1.27.150")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_1_3"]["private_ips.#"], "10.1.28.179")
+    #
+    # def test_segment_2(self):
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_2_0"]["private_ips.#"], "10.1.25.9")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_2_1"]["private_ips.#"], "10.1.26.159")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_2_2"]["private_ips.#"], "10.1.27.164")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_2_3"]["private_ips.#"], "10.1.28.14")
+    #
+    # def test_segment_3(self):
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_3_0"]["private_ips.#"], "10.1.25.143")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_3_1"]["private_ips.#"], "10.1.26.168")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_3_2"]["private_ips.#"], "10.1.27.221")
+    #     self.assertEqual(self.result['gpdb']["aws_network_interface.segment_3_3"]["private_ips.#"], "10.1.28.246")
 
     def test_name_suffix_master_sg(self):
-        self.assertEqual(self.result['gpdb']["aws_security_group.master_sg"]["tags.Name"], "sg-master-gpdb-apps-preprod-dq")
+        self.assertEqual(self.result['gpdb']["aws_security_group.master_sg"]["tags.Name"],
+                         "sg-master-gpdb-apps-preprod-dq")
 
     def test_name_prefix_segment_sg(self):
-        self.assertEqual(self.result['gpdb']["aws_security_group.segment_sg"]["tags.Name"], "sg-segment-gpdb-apps-preprod-dq")
+        self.assertEqual(self.result['gpdb']["aws_security_group.segment_sg"]["tags.Name"],
+                         "sg-segment-gpdb-apps-preprod-dq")
+
 
 if __name__ == '__main__':
     unittest.main()
